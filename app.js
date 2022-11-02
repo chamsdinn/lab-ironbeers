876887d3fc2d2c683d9d68ec0526d4aff14e02f1
chamsdinn <chams86@msn.com>
2022-11-02 17:58:39 +0100

    almost done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const { getRandomValues } = require('crypto');
 const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -21,5 +23,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  try {
+   const beersFromApi = await punkAPI.getBeers();
+
+    console.log('Beers from the database: ', beersFromApi);
+
+    res.render('beers', {beersFromApi : beersFromApi})
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/random-beer', async (req, res) => {
+  try{
+    randomBeer = await punkAPI.getRandom();
+
+    res.render('random-beer', {randomBeer : randomBeer}, {style :[styles.css]})
+  }catch (error) {
+    console.log(error);
+    }
+  });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
